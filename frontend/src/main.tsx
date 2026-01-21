9c65feb2500f791111830a66ab9bc9a49c037579
Diegothx <diegothx@live.com>
2026-01-21 18:51:47 -0300

    refactor: move frontend from js to tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')! // <-- the '!' tells TS it's not null
+);
 root.render(
   <React.StrictMode>
     <App />
