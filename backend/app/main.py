c7b99b92dd495bb61e82c89d21c42b80e21a568a
Diegothx <diegothx@live.com>
2026-02-05 23:32:40 -0300

    fix: add admin routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,15 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.api.v1.public import   health, quotes  # Import quotes router
+from app.api.v1.admin import login, quotes as adminQuotes  # Ensure admin login routes are included
 import os
 
 app = FastAPI(title="Ark Backend")
 
-app.include_router(health.router) 
+app.include_router(health.router)
 app.include_router(quotes.router)
+app.include_router(login.router)
+app.include_router(adminQuotes.router)
 
 # Detect if running in dev
 ENV = os.getenv("ENV", "development")
